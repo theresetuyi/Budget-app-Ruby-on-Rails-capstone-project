75c5b78446763d35cdaa2e8936ff5bc1c46dba23
unknown <teddytiyi@gmail.com>
2023-09-14 13:39:43 +0200

    add Ruby on Rails capstone project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  devise_for :users
+  resources :categories do
+    resources :transactions
+  end
+  root 'home#index'
 end
